076f9697d5ae6ed169d912009dbdd8a000aedcd8
Curt Micol <asenchi@asenchi.com>
2023-04-02 21:58:21 -0400

    Important keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- someday figure this out in lua
+vim.cmd('map <leader>a  :e <C-R>=expand("%:p:h") . "/"<CR>')
