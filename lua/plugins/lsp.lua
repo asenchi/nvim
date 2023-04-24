4977ec3569d407252db7d0a86462f86278e63737
Curt Micol <asenchi@asenchi.com>
2023-04-24 05:55:16 -0400

    Turn off annoying yaml stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,6 +64,13 @@ return {
             },
           },
         },
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
