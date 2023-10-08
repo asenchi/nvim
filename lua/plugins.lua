1f3fb010485857b4b92f893f4d76ee8dcb2f342b
Curt Micol <asenchi@asenchi.com>
2023-10-08 15:31:39 -0400

    Correct repo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,9 +64,9 @@ packer.startup(function(use)
   }
 
   use {
-    'kyazdani42/nvim-tree.lua',
+    'nvim-tree/nvim-tree.lua',
     requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
     config = requireconfig("nvim-tree"),
   }
