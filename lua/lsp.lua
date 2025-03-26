6fa8cb96ffba9f390b137c6dce6735895dcaeb64
Curt Micol <asenchi@asenchi.com>
2025-03-26 15:34:55 -0400

    lua and more config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,5 +4,18 @@ vim.lsp.enable({
   'tflint',
   'solargraph',
   'elixirls',
+  'lua-language-server',
 })
 
+-- Setup autocompletion
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.cmd("set completeopt+=noselect")
+vim.o.winborder = 'rounded'
