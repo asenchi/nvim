e20aaf0e157db581c4dcfe9b865adae4b98af68e
Curt Micol <asenchi@asenchi.com>
2025-04-16 08:33:43 -0400

    some things

---
+++ lua/asenchi/cmds.lua
@@ -0,0 +1,13 @@
+-- Search Google
+vim.api.nvim_create_user_command('Google', function(o)
+  local escaped = vim.uri_encode(o.args)
+  local url = ('https://www.google.com/search?q=%s'):format(escaped)
+  vim.ui.open(url)
+end, { nargs = 1, desc = "Google search" })
+
+
+vim.api.nvim_create_user_command('Bible', function(o)
+  local escaped = vim.uri_encode(o.args)
+  local url = ('https://www.biblegateway.com/passage/?search=%s&version=NIV;CSB'):format(escaped)
+  vim.ui.open(url)
+end, { nargs = 1, desc = "BibleGateway search" })
