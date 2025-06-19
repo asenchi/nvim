ad29f217788e1ece5cbff407872ac8114b29e8ce
Curt Micol <asenchi@asenchi.com>
2025-06-19 11:40:14 -0400

    add colors to diagnostics

--- init.lua
+++ init.lua
@@ -12,6 +12,14 @@ vim.cmd([[
   highlight Comment gui=italic
   highlight Constant guifg=#999999
   highlight Normal ctermbg=none guibg=none
+  highlight DiagnosticError guifg=red
+  highlight DiagnosticWarn guifg=orange
+  highlight DiagnosticInfo guifg=blue
+  highlight DiagnosticHint guifg=green
+  highlight DiagnosticFloatingError guibg=#222222 guifg=red
+  highlight DiagnosticFloatingWarn  guibg=#222222 guifg=orange
+  highlight DiagnosticFloatingInfo  guibg=#222222 guifg=blue
+  highlight DiagnosticFloatingHint  guibg=#222222 guifg=green
 ]])
 -- set bg=dark
 -- highlight NormalFloat guibg=#333333
