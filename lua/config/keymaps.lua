e7d8560c0d48d7bcf0cdc3a51045aca99b3f28b0
Curt Micol <asenchi@asenchi.com>
2025-07-07 10:21:19 -0400

    nope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -74,14 +74,3 @@ local blink = function()
   end
 end
 nmap('<leader>cb', blink)
-
-vim.keymap.set("n", "<space>c", function()
-  vim.ui.input({}, function(c)
-    if c and c~="" then
-      vim.cmd("noswapfile vnew")
-      vim.bo.buftype = "nofile"
-      vim.bo.bufhidden = "wipe"
-      vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c))
-    end
-  end)
-end)
